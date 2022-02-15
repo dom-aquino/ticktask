c2f8b46de53aba3940c10c73445a5196682f1789
Dominique Ian Aquino <dominiqueian.a@192.168.254.105>
2022-02-15 14:20:43 +0800

    Initial commit

---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    sign_in = SubmitField("Sign In")
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[
+        DataRequired(), Length(min=5, max=20)
+    ])
+    password = PasswordField("Password", validators=[
+        DataRequired(), EqualTo("repeat", message="Password must match.")
+    ])
+    repeat = PasswordField('Repeat Password')
+    register = SubmitField("Register")
+
---
+++ app/routes.py
@@ -0,0 +1,52 @@
+from app import app, db
+from app.forms import LoginForm, RegisterForm
+from app.models import User
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_user, logout_user, current_user
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("The username or password is invalid/incorrect. Try again.")
+        else:
+            login_user(user)
+            flash("Login successful.")
+            return redirect(url_for("home"))
+    return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None:
+            user = User(username=form.username.data)
+            user.set_password(form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            flash("Registration successful.")
+            return redirect(url_for("index"))
+        else:
+            flash("Username already exists. Please try again.")
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    return render_template("home.html", title="Home", user=current_user)
+
